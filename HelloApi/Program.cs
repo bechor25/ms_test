8c958b3a288ef9d2178a47cccdf5b4b032523313
Bechor Simhaev <48318320+bechor25@users.noreply.github.com>
2025-06-30 18:02:47 +0300

    Update Program.cs

--- HelloApi/Program.cs
+++ HelloApi/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok(new { message = "Hello from .NET 8!" }));
+app.MapGet("/", () => Results.Ok(new { message = "Hello from .NET 8! my name is bechor" }));
 
 app.Run();
